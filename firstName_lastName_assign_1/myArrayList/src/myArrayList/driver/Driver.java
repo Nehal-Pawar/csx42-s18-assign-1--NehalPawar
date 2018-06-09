91a22a211f4c6d8af6928f848bfd87d4ed0bb1da
Nehal Pawar <npawar3@binghamton.edu>
2018-06-09 15:56:27 -0400

    constructor fixed with default initilizer, insertion within given range,display toString but with concatenation, find size of array

--- firstName_lastName_assign_1/myArrayList/src/myArrayList/driver/Driver.java
+++ firstName_lastName_assign_1/myArrayList/src/myArrayList/driver/Driver.java
@@ -1,28 +1,30 @@
 package myArrayList.driver;
 import myArrayList.util.FileProcessor;
-public class Driver 
+import myArrayList.MyArrayList;
+public class Driver
 {
-
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-	    // command line validation is missing here. FIXME!
-	    if(2!=args.length)
+		// command line validation is missing here. FIXME!
+		if (2 != args.length)
 		{
-		    System.err.println("incorrect args passed, Expected <input.txt> <output.txt>. \n exiting \n");
-		    System.exit(0);		    
+			System.err.println("incorrect args passed, Expected <input.txt> <output.txt>. \n exiting \n");
+			System.exit(0);
 		}
-	    String INPUTFILE= args[0];
-	    String OUTPUTFILE= args[1];
-
-	    FileProcessor F1 = new FileProcessor();
-	    F1.openFile(INPUTFILE);
-		String line = "";
-			while (line != null) {
-
-				line = F1.readLine();
-				 System.out.println(line);
-			}
-	    System.out.println("Hello World: design pattern  " + INPUTFILE + ", " + OUTPUTFILE  );
+		String INPUTFILE = args[0];
+		String OUTPUTFILE = args[1];
+		MyArrayList Obj1 = new MyArrayList();
+		FileProcessor F1 = new FileProcessor();
+		F1.openFile(INPUTFILE);
+		String number = "";
+		while (number != null) {
+			number = F1.readLine();
+			if (number == null || number == "")break;
+			Obj1.insertSorted(Integer.parseInt(number));
+		}
+		Obj1.sortArray();
+		int A = Obj1.sum();
+		System.out.println("\nsum is : " + A);
+		System.out.println(Obj1.toString());
 	}
-	
 }
